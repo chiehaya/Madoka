d2fcac62185807774ad9187a5e0f4d9ffb3f0b2a
chiehaya <jakkiriko@yahoo.co.jp>
2024-03-20 18:31:00 +0900

    20240320

--- WordPressTheme/assets/js/script.js
+++ WordPressTheme/assets/js/script.js
@@ -1,4 +1,22 @@
 "use strict";
 
-jQuery(function ($) {// この中であればWordpressでも「$」が使用可能になる
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
+  var hamburger = $(".js-hamburger");
+  var close = $(".js-close");
+  hamburger.on("click", function () {
+    if ($(this).hasClass('is-open')) {
+      $('.js-sp-nav').fadeOut();
+      $(this).removeClass('is-open');
+      $(close).removeClass('is-open');
+    } else {
+      $('.js-sp-nav').fadeIn();
+      $(this).addClass('is-open');
+      $(close).addClass('is-open');
+    }
+  });
+  close.on("click", function () {
+    $('.js-sp-nav').fadeOut();
+    $(hamburger).removeClass('is-open');
+  });
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,22 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+    const hamburger = $(".js-hamburger");
+    const close = $(".js-close");
 
+    hamburger.on("click", function () {
+    if ($(this).hasClass('is-open')){
+        $('.js-sp-nav').fadeOut();
+        $(this).removeClass('is-open');
+        $(close).removeClass('is-open');
+    }else {
+        $('.js-sp-nav').fadeIn();
+        $(this).addClass('is-open');
+        $(close).addClass('is-open');
+    }
+});
 
+close.on("click", function () {
+        $('.js-sp-nav').fadeOut();
+        $(hamburger).removeClass('is-open');
+});
 });
